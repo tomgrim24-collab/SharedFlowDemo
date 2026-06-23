2fcb7417351fedde83a88cbb40d15f3526675528
composedemo <tomgrim24@gmail.com>
2026-06-24 04:10:25 +0700

    Add data collection and LazyColumn display

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
+import kotlinx.coroutines.flow.SharedFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,19 +37,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScreenSetup(modifier: Modifier = Modifier) {
-    MainScreen(modifier)
+fun ScreenSetup(
+    modifier: Modifier = Modifier,
+    viewModel: DemoViewModel = viewModel()
+) {
+    MainScreen(modifier, viewModel.sharedFlow)
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    // Здесь будет код
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    sharedFlow: SharedFlow<Int>
+) {
+    // Список для хранения полученных значений
+    val messages = remember { mutableStateListOf<Int>() }
+
+    // Сбор данных из потока
+    LaunchedEffect(key1 = Unit) {
+        sharedFlow.collect { value ->
+            messages.add(value)
+        }
+    }
+
+    // Отображение списка
+    LazyColumn(modifier = modifier) {
+        items(messages) { value ->
+            Text(
+                text = "Collected Value = $value",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SharedFlowDemoTheme {
-        ScreenSetup()
+        val viewModel: DemoViewModel = viewModel()
+        MainScreen(sharedFlow = viewModel.sharedFlow)
     }
 }
